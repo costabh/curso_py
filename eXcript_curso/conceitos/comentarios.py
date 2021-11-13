eadf0455a77fc843526c9eda74dccaff3eaf0830
Ben-Hur Costa <benhur.costa89@gmail.com>
2021-11-13 17:11:31 -0300

    enviando arquivos

---
+++ eXcript_curso/conceitos/comentarios.py
@@ -0,0 +1,10 @@
+print("aprendendo como utilizar comentarios")
+#print("segunda linha")
+print("terceira linha")
+
+# -> comentario de linha
+''' comentario de documentação'''
+"""
+comentario de bloco
+"""
+print("fim dos comentarios")
---
+++ eXcript_curso/conceitos/identacao.py
@@ -0,0 +1,11 @@
+print(1+1)
+#se der um espaço 
+ #print(1+1)
+#causara erro
+
+if True:
+    #indentação correta
+
+
+#outra forma não recomendada
+print("oi");print("tchau");
